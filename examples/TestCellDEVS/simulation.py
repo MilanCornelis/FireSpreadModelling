8d5329b9cc66e8b895eb8c3f47805859374a15fc
MilanCornelis <milan.cornelis@hotmail.com>
2021-03-11 18:05:27 +0100

    first try to create a cell, doesn't work yet

--- examples/TestCellDEVS/simulation.py
+++ examples/TestCellDEVS/simulation.py
@@ -2,7 +2,11 @@ from pypdevs.simulator import Simulator
 from coupledModel import CellSpace
 
 
+# Cell devs doesn't work
 sim = Simulator(CellSpace())
+sim.cell = True
+sim.cell_file = "celltrace"
+sim.cell_multifile = False
 sim.setVerbose()
 sim.setTerminationTime(2000)
 sim.setClassicDEVS()
