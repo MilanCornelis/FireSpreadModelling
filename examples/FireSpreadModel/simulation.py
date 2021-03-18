e7d0368916316e7b5386d89ae720651df8902b11
MilanCornelis <milan.cornelis@hotmail.com>
2021-03-18 17:08:41 +0100

    Read settings from file

--- examples/FireSpreadModel/simulation.py
+++ examples/FireSpreadModel/simulation.py
@@ -1,13 +1,18 @@
 from pypdevs.simulator import Simulator
 from coupledModel import CellSpace
+import ConfigParser
 
-x = 11
-y = 11
-burn_x = 8
-burn_y = 5
-temperature = 100
+config = ConfigParser.ConfigParser()
+config.read("settings.txt")
+x = int(config.get("myvars", "x"))
+y = int(config.get("myvars", "y"))
+burn_x = int(config.get("myvars", "burn_x"))
+burn_y = int(config.get("myvars", "burn_y"))
+temperature = int(config.get("myvars", "temperature"))
+wind_speed = int(config.get("myvars", "wind_speed"))
+wind_dir = int(config.get("myvars", "wind_dir"))
+
 sim = Simulator(CellSpace(x, y, burn_x, burn_y, temperature))
-
 sim.setTerminationTime(100)
 sim.setCell(x, y, cell_file="./simout/celltrace", multifile=False)
 sim.simulate()
