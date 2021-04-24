28b39337596ed323c6c40f3252c988a9b7038046
MilanCornelis <milan.cornelis@hotmail.com>
2021-04-24 10:19:58 +0200

    Commit

--- examples/FireSpreadModel/simulation.py
+++ examples/FireSpreadModel/simulation.py
@@ -15,7 +15,7 @@ wind_dir = int(config.get("myvars", "wind_dir"))
 
 sim = Simulator(CellSpace(x, y, burn_x, burn_y, temperature, wind_dir, wind_speed))
 sim.setTerminationTime(750)
-#sim.setCell(x, y, cell_file="./simout/celltrace", multifile=False) #
+#sim.setCell(x, y, cell_file="./simout/celltrace", multifile=False)
 sim.setCell(x, y, cell_file="./simout/celltrace-%05d", multifile=True)
 sim.simulate()
 print("Simulation finished")
