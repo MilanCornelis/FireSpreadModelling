037c7f03b0dcf7d1c3b915aeb7988239d3d04f7a
MilanCornelis <milan.cornelis@hotmail.com>
2021-04-29 14:44:44 +0200

    Test with wind dir 0 and 5m/s

--- examples/FireSpreadModel/coupledModel.py
+++ examples/FireSpreadModel/coupledModel.py
@@ -15,11 +15,12 @@ class CellSpace(CoupledDEVS):
                 if x == burn_x and y == burn_y:
                     row.append(self.addSubModel(BurningCell(x, y, 125, 50.0, wind_dir, wind_speed)))
                 else:
-                    # Introduce some non-burnable cells
+                    row.append(self.addSubModel(Cell(x, y, temperature, 50.0, wind_dir, wind_speed)))
+                    """# Introduce some non-burnable cells
                     if (24 <= y <= 28) and (20 <= x <= 25):
                         row.append(self.addSubModel(Cell(x, y, 50, 0.0, wind_dir, wind_speed)))
                     else:
-                        row.append(self.addSubModel(Cell(x, y, temperature, 50.0, wind_dir, wind_speed)))
+                        row.append(self.addSubModel(Cell(x, y, temperature, 50.0, wind_dir, wind_speed)))"""
             cells.append(row)
         print("Creating cell space: END")
 
